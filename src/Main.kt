c5e81398da5598eec964cd3524915be40c693e5b
Dieter <dieter.decaestecker@test.com>
2020-04-18 21:20:00 +0200

    Habemus rotating triangle

--- src/Main.kt
+++ src/Main.kt
@@ -13,7 +13,7 @@ private fun createUnitCube(): Mesh {
     return Mesh(
         listOf(
             Triangle(Vector3D(0f, 0f, 0f), Vector3D(0f, 1f, 0f), Vector3D(1f, 1f, 0f)),
-            Triangle(Vector3D(0f, 0f, 0f), Vector3D(1f, 0f, 0f), Vector3D(1f, 0f, 0f)),
+            Triangle(Vector3D(0f, 0f, 0f), Vector3D(1f, 1f  , 0f), Vector3D(1f, 0f, 0f)),
 
             Triangle(Vector3D(1f, 0f, 0f), Vector3D(1f, 1f, 0f), Vector3D(1f, 1f, 1f)),
             Triangle(Vector3D(1f, 0f, 0f), Vector3D(1f, 1f, 1f), Vector3D(1f, 0f, 1f)),
